22eaa4f2c341c522456736b90f33475216ea2dc4
Ty Stahnke <pstahnke@ucsd.edu>
2016-02-12 18:08:01 -0800

    API 1.0

---
+++ YahooFinanceAPI/src/getquote.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+
+public class getquote {
+
+	public static void main (String args[]) throws IOException
+	{
+		
+		Stock stock = YahooFinance.get("INTC");
+		 
+		BigDecimal price = stock.getQuote().getPrice();
+		BigDecimal change = stock.getQuote().getChangeInPercent();
+		BigDecimal peg = stock.getStats().getPeg();
+		BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
+		 
+		stock.print();		
+	}
+}
+

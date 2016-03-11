91733748cf86022347cb00ccb62c6eee3cbaf5a8
Ty Stahnke <pstahnke@ucsd.edu>
2016-03-11 13:45:18 -0800

    getquote.java commented

--- app/src/main/java/com/example/quick619/project/getquote.java
+++ app/src/main/java/com/example/quick619/project/getquote.java
@@ -6,8 +6,23 @@ import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 
+/**
+ * Created by Ty Stahnke
+ *
+ * Class that handles access the API
+ * Used to get current price, current change, and the company name
+ * All methods have checks that prevent user from inputting unaccepted input to API
+ */
+
 public class getquote {
 
+	/**
+	 * getprice()
+	 *
+	 * @param symbol Ticker symbol to get price for e.g. "AAPL" for Apple Inc.
+	 * @return price from API
+	 * @throws IOException
+	 */
 	public double getprice (String symbol) throws IOException
 	{
 
@@ -34,6 +49,13 @@ public class getquote {
 		return price_dub;
 	}
 
+	/**
+	 * getchange()
+	 *
+	 * @param symbol Ticker symbol to get price for e.g. "AAPL" for Apple Inc.
+	 * @return change in price from API
+	 * @throws IOException
+	 */
 	public double getchange (String symbol) throws IOException
 	{
 
@@ -54,6 +76,13 @@ public class getquote {
 		return change_dub;
 	}
 
+	/**
+	 * getname()
+	 *
+	 * @param symbol Ticker symbol to get price for e.g. "AAPL" for Apple Inc.
+	 * @return Comapny Name from API
+	 * @throws IOException
+	 */
 	public String getname (String symbol) throws IOException
 	{
 
@@ -72,7 +101,7 @@ public class getquote {
 		return name;
 	}
 
-
+	/**Helper method for determining if the inputted symbol contains only letters*/
 	public boolean isAlpha(String name) {
 		char[] chars = name.toCharArray();
 

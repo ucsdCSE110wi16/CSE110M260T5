d9acabedb3d4e88bc5df7c4c79d0d69062186b9a
Srmacam <saremacam@gmail.com>
2016-03-11 13:58:46 -0800

    Minor Commenting in DecimalDigitsInnputFilter

--- app/src/main/java/com/example/quick619/project/DecimalDigitsInputFilter.java
+++ app/src/main/java/com/example/quick619/project/DecimalDigitsInputFilter.java
@@ -22,6 +22,7 @@ public class DecimalDigitsInputFilter implements InputFilter {
         mPattern=Pattern.compile("[0-9]{0," + (digitsBeforeZero-1) + "}+((\\.[0-9]{0," + (digitsAfterZero-1) + "})?)||(\\.)?");
     }
 
+    // Stops the user from inputting more digits after two decimal places
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
 

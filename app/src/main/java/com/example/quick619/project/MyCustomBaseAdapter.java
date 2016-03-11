fda3f31dd71756f8608e1c07a90bdbd16466d7f3
Ty Stahnke <pstahnke@ucsd.edu>
2016-03-11 13:54:15 -0800

    MyCustomerBaseAdapter.java commented

--- app/src/main/java/com/example/quick619/project/MyCustomBaseAdapter.java
+++ app/src/main/java/com/example/quick619/project/MyCustomBaseAdapter.java
@@ -13,30 +13,35 @@ import java.util.ArrayList;
 
 /**
  * Created by Ty on 2/26/2016.
+ * Custom adapter for creating the list of stocks ont he main_activity page
+ * ListView with 3 text fields: Name, Price, Change
  */
 public class MyCustomBaseAdapter extends BaseAdapter {
-    private static ArrayList<ActiveStock> searchArrayList;
+    private static ArrayList<ActiveStock> stockArrayList;
 
     private LayoutInflater mInflater;
 
-    public MyCustomBaseAdapter(Context context, ArrayList<ActiveStock> results) {
-        searchArrayList = results;
+    /**Constructor*/
+    public MyCustomBaseAdapter(Context context, ArrayList<ActiveStock> list) {
+        stockArrayList = list;
         mInflater = LayoutInflater.from(context);
     }
 
+    /**Get arraylist size*/
     public int getCount() {
-        return searchArrayList.size();
+        return stockArrayList.size();
     }
-
+    /**Get the item at the given position*/
     public Object getItem(int position) {
-        return searchArrayList.get(position);
+        return stockArrayList.get(position);
     }
-
+    /**get the item Id of the item*/
     public long getItemId(int position) {
         return position;
     }
-
+    /**Get the view for each item*/
     public View getView(int position, View convertView, ViewGroup parent) {
+        /**Get a new ViewHolder and get the views within it*/
         ViewHolder holder;
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.custom_row_view, null);
@@ -45,42 +50,48 @@ public class MyCustomBaseAdapter extends BaseAdapter {
             ImageView editButton = (ImageView) convertView.findViewById(R.id.stockInfo);
             editButton.setTag(position);
 
-            holder.txtName = (TextView) convertView.findViewById(R.id.name);
-            holder.txtCityState = (TextView) convertView.findViewById(R.id.main_price);
-            holder.txtPhone = (TextView) convertView.findViewById(R.id.main_change);
+            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.price = (TextView) convertView.findViewById(R.id.main_price);
+            holder.change = (TextView) convertView.findViewById(R.id.main_change);
 
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        String sign = "";
+        /**Set the views of view holder*/
 
-        if(searchArrayList.get(position).getChange() > 0){
-            holder.txtPhone.setTextColor(Color.GREEN);
+        /**Set the color and sign of change depending on whether it's -, 0 or +*/
+        String sign;
+
+        /**Positive*/
+        if(stockArrayList.get(position).getChange() > 0){
+            holder.change.setTextColor(Color.GREEN);
             sign = "+";
         }
-        else if(searchArrayList.get(position).getChange() < 0) {
-            holder.txtPhone.setTextColor(Color.RED);
+        /**Negative*/
+        else if(stockArrayList.get(position).getChange() < 0) {
+            holder.change.setTextColor(Color.RED);
             sign = "";
         }
-
+        /**Zero*/
         else {
-            holder.txtPhone.setTextColor(Color.GRAY);
+            holder.change.setTextColor(Color.GRAY);
             sign = "";
         }
 
-        holder.txtName.setText(searchArrayList.get(position).getTicker());
-        holder.txtCityState.setText("$" + Double.toString(searchArrayList.get(position).getPrice()));
-        holder.txtPhone.setText(sign + Double.toString(searchArrayList.get(position).getChange()));
+        /**Finally, set the text of each view*/
+        holder.name.setText(stockArrayList.get(position).getTicker());
+        holder.price.setText("$" + Double.toString(stockArrayList.get(position).getPrice()));
+        holder.change.setText(sign + Double.toString(stockArrayList.get(position).getChange()));
 
         return convertView;
     }
 
+    /**View holder class, holds the TextViews in the customer view*/
     static class ViewHolder {
-        TextView txtName;
-        TextView txtCityState;
-        TextView txtPhone;
-        ImageView editButton;
+        TextView name;
+        TextView price;
+        TextView change;
     }
 }
